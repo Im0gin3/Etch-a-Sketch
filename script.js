6b0b5b27b4c07e6e37cec4ebed9fa5e31980c729
Im0gin3 <113782157+Im0gin3@users.noreply.github.com113782157+Im0gin3@users.noreply.github.com>
2025-08-02 12:16:17 +0530

    Let user change number of squares

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 const container = document.querySelector(".container");
-for (let outer=0;outer<16;outer++){
-    for (let inner=0;inner<16;inner++){
+const button = document.querySelector("#changeSquares");
+let numberSquares=16;
+
+
+button.addEventListener("click", ()=>{
+    numberSquares=prompt("How many squares do you want per side in the new grid?");
+    while (numberSquares>100){
+        numberSquares=prompt("How many squares do you want per side in the new grid?");
+    }
+    document.querySelectorAll(".square").forEach(el => el.remove());
+    let width = 100/numberSquares;
+    for (let outer=0;outer<numberSquares;outer++){
+    for (let inner=0;inner<numberSquares;inner++){
+        const div = document.createElement("div");
+        div.classList.add("square");
+        container.appendChild(div);
+        div.style.cssText=`width: ${width}vw; height: ${width}vw;`;
+    }
+    document.querySelectorAll(".square").forEach((div) => {
+    div.onmouseover = (event) => {
+    div.classList.add("hovering");
+  }
+})
+}
+})
+for (let outer=0;outer<numberSquares;outer++){
+    for (let inner=0;inner<numberSquares;inner++){
         const div = document.createElement("div");
         div.classList.add("square")
         container.appendChild(div);
